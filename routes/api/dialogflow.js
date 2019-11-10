d86277193b5a270708fed05e3eb24ad87353a705
Abir Taheer <abir488@gmail.com>
2019-11-10 11:14:39 -0500

    extra body info

--- routes/api/dialogflow.js
+++ routes/api/dialogflow.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.post("/api/dialogflow", (req, res) => {
     let response = {
-        "fulfillmentText": "Ok buddy"
+        "fulfillmentText": JSON.stringify(req.body)
     };
 
     res.send(JSON.stringify(response));
