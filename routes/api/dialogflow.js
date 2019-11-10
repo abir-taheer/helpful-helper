73b0e260b5daf95a94ef9382c36ea2e1b93ca8dd
Abir Taheer <abir488@gmail.com>
2019-11-10 11:00:35 -0500

    new fulfillment

--- routes/api/dialogflow.js
+++ routes/api/dialogflow.js
@@ -1,7 +1,54 @@
 const router = require("express").Router();
 
 router.get("/api/dialogflow", (req, res) => {
-    let response = {"fulfillmentText": "hi"};
+    let response = {
+        "fulfillmentText": "displayed&spoken response",
+        "fulfillmentMessages": [
+            {
+                "text": [
+                    "text response"
+                ],
+            }
+        ],
+        "source": "example.com",
+        "payload": {
+            "google": {
+                "expectUserResponse": true,
+                "richResponse": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": "this is a simple response"
+                            }
+                        }
+                    ]
+                }
+            },
+            "facebook": {
+                "text": "Hello, Facebook!"
+            },
+            "slack": {
+                "text": "This is a text response for Slack."
+            }
+        },
+        "outputContexts": [
+            {
+                "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+                "lifespanCount": 5,
+                "parameters": {
+                    "param": "param value"
+                }
+            }
+        ],
+        "followupEventInput": {
+            "name": "event name",
+            "languageCode": "en-US",
+            "parameters": {
+                "param": "param value"
+            }
+        }
+    };
+
 
     res.send(JSON.stringify(response));
 });
