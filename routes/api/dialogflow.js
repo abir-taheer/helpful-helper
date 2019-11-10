ef73187c54a784b7e743d941404dd64ffc448ee7
Abir Taheer <abir488@gmail.com>
2019-11-10 10:48:57 -0500

    simplified google thing

--- routes/api/dialogflow.js
+++ routes/api/dialogflow.js
@@ -1,30 +1,7 @@
 const router = require("express").Router();
 
 router.get("/api/dialogflow", (req, res) => {
-    let response = {
-        "fulfillmentText": "displayed&spoken response",
-        "fulfillmentMessages": [
-            {
-                "text": [
-                    "text response"
-                ],
-            }
-        ],
-        "payload": {
-            "google": {
-                "expectUserResponse": true,
-                "richResponse": {
-                    "items": [
-                        {
-                            "simpleResponse": {
-                                "textToSpeech": "this is a simple response"
-                            }
-                        }
-                    ]
-                }
-            }
-        }
-    };
+    let response = {"fulfillmentText": "hi"};
 
     res.json(response);
 
