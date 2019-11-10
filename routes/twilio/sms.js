d32b5d6f40d97ecf41f334287325c5e88188e539
Abir Taheer <abir488@gmail.com>
2019-11-10 04:29:39 -0500

    more useful name

--- routes/twilio/sms.js
+++ routes/twilio/sms.js
@@ -5,14 +5,14 @@ router.post('/sms', async function(req, res) {
     let from = req.body.From.replace("+1", "");
     let message = req.body.Body;
 
-    let user = await User.getByPhone(from);
+    let user_id = await User.getByPhone(from);
 
-    if(user){
+    if(user_id){
         if(message.toLowerCase().startsWith("add")){
-            let request = message.replace("add", "").trim();
+            let request = message.substr(3).trim();
             let date = new Date();
             date.setDate(date.getDate() + 1);
-            await User.addTodo(user.user_id, request, date);
+            await User.addTodo(user_id, request, date);
             res.send(`<Response><Message>${request} has been added to your todo list!</Message></Response>`);
         }
 
